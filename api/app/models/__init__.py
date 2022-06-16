f6300943284e450ed62c3c6503e467312a731160
Luciano Righetti <luciano.righetti@gmail.com>
2022-06-16 11:34:34 +0200

    fix: sqlalchemy needs to load all models when in declarative mode

---
+++ api/app/models/__init__.py
@@ -0,0 +1,8 @@
+from .attribute import Attribute  # noqa
+from .event import Event  # noqa
+from .object import Object  # noqa
+from .role import Role  # noqa
+from .server import Server  # noqa
+from .user import User  # noqa
+
+# fixes: sqlalchemy.exc.InvalidRequestError: When initializing mapper mapped class AAA->bbb, expression 'XXXX' failed to locate a name ('XXXX'). If this is a class name, consider adding this relationship() to the <class 'app.models.bbb.AAAA'> class after both dependent classes have been defined.
