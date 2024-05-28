5b132ecf877e309eb5361a394c32aa2310422600
Luciano Righetti <luciano.righetti@gmail.com>
2024-05-28 15:47:31 +0200

    fix: remove commented sample code

--- api/app/opensearch.py
+++ api/app/opensearch.py
@@ -13,25 +13,3 @@ OpenSearchClient = OpenSearch(
     verify_certs=False,
     ssl_show_warn=False,
 )
-
-# # Sample data to be indexed
-# data = {
-#     'title': 'Sample Document',
-#     'content': 'This is a sample document to be indexed in OpenSearch.',
-#     'author': 'John Doe',
-#     'timestamp': '2023-05-24T12:34:56'
-# }
-
-# # Index the document
-# response = client.index(
-#     index="test",
-#     id=1,
-#     body=data
-# )
-
-# # Check the response
-# if response['result'] == 'created':
-#     print('Document indexed successfully')
-# else:
-#     print(f'Failed to index document. Result: {response["result"]}')
-#     print(response)
