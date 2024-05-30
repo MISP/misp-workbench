fec9d25f03b5fcdc7a6be3578a5a0e1d9e7f40bc
Luciano Righetti <luciano.righetti@gmail.com>
2024-05-30 15:54:57 +0200

    fix: update submodules in entrypoint

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # run migrations
 poetry run alembic upgrade head
 
+# pull submodules
+git submodule update --init --recursive
+
 # start API
 poetry run uvicorn app.main:app --host 0.0.0.0 --port 80
