775c5c0b8be6de0fcaefc9745839b93023ad7f0c
Luciano Righetti <luciano.righetti@gmail.com>
2024-07-16 11:25:35 +0200

    chg: use datalist

--- frontend/src/components/enums/ObjectTemplateSelect.vue
+++ frontend/src/components/enums/ObjectTemplateSelect.vue
@@ -13,14 +13,15 @@ const { objectTemplates } = storeToRefs(objectsStore);
 objectsStore.getObjectTemplates();
 
 function handleSelectChange(event) {
+    console.log(event.target.value);
     emit('object-template-updated', event.target.value);
 }
 </script>
 
 <template>
-    <Field class="form-control" :name="name" :class="{ 'is-invalid': errors }" as="select" @change="handleSelectChange"
-        :value="props.selected">
-        <option v-for="(template) in objectTemplates" :value="template.uuid">{{ template.name }}
-        </option>
-    </Field>
+    <Field class="form-control" list="objectTemplateOptions" id="objectTemplatesSelect" :name="name" :class="{ 'is-invalid': errors }"  @change="handleSelectChange"
+    placeholder="Type to search..." :value="props.selected.uuid"></Field>
+    <datalist id="objectTemplateOptions" >
+        <option v-for="template in objectTemplates" :value="template.uuid" :uuid="template.uuid">{{ template.name }}</option>
+    </datalist>
 </template>
