ace194db35399ce77b8a93e3d47579c31b726b87
Luciano Righetti <luciano.righetti@gmail.com>
2025-02-05 15:57:34 +0100

    fix: minioclient

--- api/app/miniocli.py
+++ api/app/miniocli.py
@@ -4,13 +4,16 @@ from app.settings import get_settings
 
 Settings = get_settings()
 
-MinioClient = Minio(
-    Settings.Storage.minio.host,
-    access_key=Settings.Storage.minio.access_key,
-    secret_key=Settings.Storage.minio.secret_key,
-    secure=Settings.Storage.minio.secure,
-)
+if Settings.Storage.engine == "minio":
+    MinioClient = Minio(
+        Settings.Storage.minio.host,
+        access_key=Settings.Storage.minio.access_key,
+        secret_key=Settings.Storage.minio.secret_key,
+        secure=Settings.Storage.minio.secure,
+    )
+    found = MinioClient.bucket_exists(Settings.Storage.minio.bucket)
 
-found = MinioClient.bucket_exists(Settings.Storage.minio.bucket)
-if not found:
-    MinioClient.make_bucket(Settings.Storage.minio.bucket)
+    if not found:
+        MinioClient.make_bucket(Settings.Storage.minio.bucket)
+else:
+    MinioClient = None
