6bb107d03eaedc3d6a2cb7c61563a92ae23fcda6
Luciano Righetti <luciano.righetti@gmail.com>
2025-04-08 15:54:12 +0200

    add: create ingest pipelines

--- api/opensearch/entrypoint.sh
+++ api/opensearch/entrypoint.sh
@@ -7,9 +7,9 @@ OPENSEARCH_PORT="${OPENSEARCH_PORT:-9200}"
 OPENSEARCH_HOSTNAME="${OPENSEARCH_HOSTNAME:-opensearch}"
 OPENSEARCH_URL="$OPENSEARCH_HOSTNAME:$OPENSEARCH_PORT"
 MAPPINGS_DIR="/mappings"
+PIPELINES_DIR="/pipelines"
 
 echo "Initializing OpenSearch indices..."
-
 for file in "$MAPPINGS_DIR"/*.json; do
   INDEX_NAME=$(basename "$file" .json)
 
@@ -22,5 +22,21 @@ for file in "$MAPPINGS_DIR"/*.json; do
          -H "Content-Type: application/json" \
          -d @"$file"
     echo "Index '$INDEX_NAME' created."
+  fi
+done
+
+echo "Creating OpenSearch ingest pipelines..."
+for file in "$PIPELINES_DIR"/*.json; do
+  PIPELINE_NAME=$(basename "$file" .json)
+
+  # Check if pipeline exists
+  if curl -s -o /dev/null -w "%{http_code}" "$OPENSEARCH_URL/_ingest/pipeline/$PIPELINE_NAME" | grep -q "200"; then
+    echo "Pipeline '$PIPELINE_NAME' already exists. Skipping..."
+  else
+    echo "Creating pipeline '$PIPELINE_NAME' using mapping file '$file'..."
+    curl -s -X PUT "$OPENSEARCH_URL/_ingest/pipeline/$PIPELINE_NAME" \
+         -H "Content-Type: application/json" \
+         -d @"$file"
+    echo "Pipeline '$PIPELINE_NAME' created."
   fi
 done
