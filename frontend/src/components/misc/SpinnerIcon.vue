4acda2abb1b1b2c6a1067ab923a2a839b93a5eac
Luciano Righetti <luciano.righetti@gmail.com>
2025-06-19 14:55:42 +0200

    add: old icon spinner

---
+++ frontend/src/components/misc/SpinnerIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="spinner-overlay">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.spinner-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1050; /* Bootstrap modal default is 1050 */
+  background-color: rgba(0, 0, 0, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
