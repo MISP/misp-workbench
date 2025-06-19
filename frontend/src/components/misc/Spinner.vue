c7fdebd4ea794a00afac53ea46815eb910fcbda2
Luciano Righetti <luciano.righetti@gmail.com>
2025-06-19 14:55:20 +0200

    chg: make spinner overlay appear only if request takes more 100ms to avoid flickering

--- frontend/src/components/misc/Spinner.vue
+++ frontend/src/components/misc/Spinner.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="spinner-overlay">
+  <div v-if="visible" class="spinner-overlay">
     <div class="spinner-border" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Spinner",
+  data() {
+    return {
+      visible: false,
+      timer: null,
+    };
+  },
+  mounted() {
+    this.timer = setTimeout(() => {
+      this.visible = true;
+    }, 100);
+  },
+  beforeUnmount() {
+    clearTimeout(this.timer);
+  },
+};
+</script>
 
 <style scoped>
 .spinner-overlay {
@@ -13,8 +33,8 @@
   left: 0;
   width: 100vw;
   height: 100vh;
-  z-index: 1050; /* Bootstrap modal default is 1050 */
-  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 1050;
+  background-color: rgba(0, 0, 0, 0.7);
   display: flex;
   justify-content: center;
   align-items: center;
