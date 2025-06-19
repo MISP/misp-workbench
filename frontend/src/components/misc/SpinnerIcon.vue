1129908df00ef0ea05c6203e99b195ef3321c025
Luciano Righetti <luciano.righetti@gmail.com>
2025-06-19 15:16:28 +0200

    chg: remove overlay

--- frontend/src/components/misc/SpinnerIcon.vue
+++ frontend/src/components/misc/SpinnerIcon.vue
@@ -1,22 +1,13 @@
+<style>
+.container {
+  text-align: center;
+}
+</style>
+
 <template>
-  <div class="spinner-overlay">
-    <div class="spinner-border" role="status">
+  <div class="container">
+    <div class="spinner-border m-5" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
   </div>
 </template>
-
-<style scoped>
-.spinner-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  z-index: 1050; /* Bootstrap modal default is 1050 */
-  background-color: rgba(0, 0, 0, 0.6);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
