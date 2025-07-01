b1fee99654810244b106146979a4f28861e95bbc
Luciano Righetti <luciano.righetti@gmail.com>
2025-07-01 13:47:07 +0200

    fix: make uuid inline

--- frontend/src/components/misc/UUID.vue
+++ frontend/src/components/misc/UUID.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+
 const props = defineProps(["uuid"]);
 
 function copyUuidToClipboard() {
@@ -7,12 +10,14 @@ function copyUuidToClipboard() {
 </script>
 
 <template>
-  <span class="badge bg-light text-dark shadow-sm">{{ uuid }} </span>
-  <button type="button" class="btn">
-    <font-awesome-icon
-      class="text-primary"
-      icon="fa-solid fa-copy"
-      @click="copyUuidToClipboard"
-    />
-  </button>
+  <span class="d-inline-flex align-items-center gap-1">
+    <span class="badge bg-light text-dark shadow-sm mb-0">{{ uuid }}</span>
+    <button type="button" class="btn btn-sm p-0 border-0 bg-transparent">
+      <FontAwesomeIcon
+        class="text-primary"
+        :icon="faCopy"
+        @click="copyUuidToClipboard"
+      />
+    </button>
+  </span>
 </template>
