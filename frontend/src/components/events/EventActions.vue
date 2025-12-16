471f4c74a449a595b0faf8c7080a03ac07af7e4e
Luciano Righetti <luciano.righetti@gmail.com>
2025-12-16 12:10:45 +0100

    fix: mobile ui fixes

--- frontend/src/components/events/EventActions.vue
+++ frontend/src/components/events/EventActions.vue
@@ -4,17 +4,18 @@ import { ref, computed, onMounted } from "vue";
 import { Modal } from "bootstrap";
 import { storeToRefs } from "pinia";
 import { useAuthStore, useEventsStore } from "@/stores";
-import DeleteEventModal from "@/components/events/DeleteEventModal.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
   faTrash,
   faEye,
   faPen,
+  faEllipsisVertical,
   faFileArrowUp,
   faSync,
   faBookmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { toggleFollowEntity, isFollowingEntity } from "@/helpers/follow";
+import DeleteEventModal from "@/components/events/DeleteEventModal.vue";
 
 const authStore = useAuthStore();
 const { scopes } = storeToRefs(authStore);
@@ -81,87 +82,163 @@ function followEvent() {
 
 <style scoped>
 .btn-toolbar {
-  flex-wrap: nowrap !important;
+  display: flex;
+  gap: 0.25rem;
+}
+
+@media (max-width: 576px) {
+  .btn-toolbar {
+    flex-wrap: wrap !important;
+    width: 100%;
+  }
+
+  .btn-group,
+  .btn-group-vertical {
+    width: 100%;
+  }
+
+  .btn-group .btn,
+  .btn-group-vertical .btn {
+    width: 100%;
+    justify-content: center;
+  }
 }
 </style>
 
 <template>
-  <div class="btn-toolbar float-end" role="toolbar">
-    <div
-      :class="{ 'btn-group-vertical': $isMobile, 'btn-group me-2': !$isMobile }"
-      role="group"
-      aria-label="Event Actions"
-    >
-      <RouterLink
-        v-if="actions.view"
-        :to="`/events/${event_uuid}`"
-        class="btn btn-outline-primary btn-sm"
-        data-placement="top"
-        data-toggle="tooltip"
-        title="View Event"
-      >
-        <FontAwesomeIcon :icon="faEye" />
-      </RouterLink>
-      <RouterLink
-        v-if="actions.update"
-        :to="`/events/update/${event_uuid}`"
-        class="btn btn-outline-primary btn-sm"
-        data-placement="top"
-        data-toggle="tooltip"
-        title="Update Event"
-      >
-        <FontAwesomeIcon :icon="faPen" />
-      </RouterLink>
-    </div>
+  <div
+    v-if="!$isMobile"
+    class="btn-toolbar d-flex align-items-center flex-nowrap float-end"
+    role="toolbar"
+  >
     <div class="btn-group me-2" role="group">
       <button
         v-if="actions.index"
-        :disabled="status.indexing"
         type="button"
         class="btn btn-outline-primary btn-sm"
-        data-placement="top"
-        data-toggle="tooltip"
+        :disabled="status.indexing"
         title="Index Event"
         @click="indexEventDocument"
       >
-        <FontAwesomeIcon v-if="!status.indexing" :icon="faFileArrowUp" />
-        <FontAwesomeIcon v-if="status.indexing" :icon="faSync" spin />
+        <FontAwesomeIcon
+          v-if="!status.indexing"
+          :icon="faFileArrowUp"
+          fixed-width
+        />
+        <FontAwesomeIcon v-else :icon="faSync" fixed-width spin />
       </button>
+
       <button
         type="button"
         class="btn btn-outline-primary btn-sm"
-        data-placement="top"
-        data-toggle="tooltip"
         title="Follow Event"
         @click="followEvent"
       >
         <FontAwesomeIcon
-          v-if="!followed"
           :icon="faBookmark"
-          :inverse="true"
-          class="text-primary"
-        />
-        <FontAwesomeIcon
-          v-if="followed"
-          :icon="faBookmark"
-          class="text-success"
+          fixed-width
+          :class="followed ? 'text-success' : 'text-primary'"
         />
       </button>
     </div>
-    <div class="btn-group me-2" role="group">
+    <div class="btn-group me-2" role="group" aria-label="Event Actions">
+      <RouterLink
+        v-if="actions.view"
+        :to="`/events/${event_uuid}`"
+        class="btn btn-outline-primary btn-sm"
+        title="View Event"
+      >
+        <FontAwesomeIcon :icon="faEye" fixed-width />
+      </RouterLink>
+
+      <RouterLink
+        v-if="actions.update"
+        :to="`/events/update/${event_uuid}`"
+        class="btn btn-outline-primary btn-sm"
+        title="Update Event"
+      >
+        <FontAwesomeIcon :icon="faPen" fixed-width />
+      </RouterLink>
+
       <button
         v-if="actions.delete"
         type="button"
         class="btn btn-danger btn-sm"
-        data-placement="top"
-        data-toggle="tooltip"
         title="Delete Event"
         @click="openDeleteEventModal"
       >
-        <FontAwesomeIcon :icon="faTrash" />
+        <FontAwesomeIcon :icon="faTrash" fixed-width />
       </button>
     </div>
   </div>
+
+  <div v-else class="dropdown text-end">
+    <button
+      class="btn btn-outline-secondary btn-sm"
+      type="button"
+      data-bs-toggle="dropdown"
+      aria-expanded="false"
+      aria-label="Event actions"
+    >
+      <FontAwesomeIcon :icon="faEllipsisVertical" fixed-width />
+    </button>
+
+    <ul class="dropdown-menu dropdown-menu-end">
+      <li v-if="actions.view">
+        <RouterLink :to="`/events/${event_uuid}`" class="dropdown-item">
+          <FontAwesomeIcon :icon="faEye" fixed-width class="me-2" />
+          View
+        </RouterLink>
+      </li>
+
+      <li v-if="actions.update">
+        <RouterLink :to="`/events/update/${event_uuid}`" class="dropdown-item">
+          <FontAwesomeIcon :icon="faPen" fixed-width class="me-2" />
+          Edit
+        </RouterLink>
+      </li>
+
+      <li v-if="actions.index">
+        <button
+          class="dropdown-item"
+          :disabled="status.indexing"
+          @click="indexEventDocument"
+        >
+          <FontAwesomeIcon
+            :icon="status.indexing ? faSync : faFileArrowUp"
+            fixed-width
+            class="me-2"
+            :spin="status.indexing"
+          />
+          Index
+        </button>
+      </li>
+
+      <li>
+        <button class="dropdown-item" @click="followEvent">
+          <FontAwesomeIcon
+            :icon="faBookmark"
+            fixed-width
+            class="me-2"
+            :class="followed ? 'text-success' : 'text-primary'"
+          />
+          {{ followed ? "Unfollow" : "Follow" }}
+        </button>
+      </li>
+
+      <li>
+        <hr class="dropdown-divider" />
+      </li>
+
+      <li v-if="actions.delete">
+        <button class="dropdown-item text-danger" @click="openDeleteEventModal">
+          <FontAwesomeIcon :icon="faTrash" fixed-width class="me-2" />
+          Delete
+        </button>
+      </li>
+    </ul>
+  </div>
+
   <DeleteEventModal
     :key="event_uuid"
     :id="`deleteEventModal_${event_uuid}`"
