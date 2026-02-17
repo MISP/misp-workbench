4d91e2fca018684f661b5a23936f2d0051da97fc
Luciano Righetti <luciano.righetti@gmail.com>
2026-02-17 15:50:09 +0100

    fix: import

--- api/app/services/redis.py
+++ api/app/services/redis.py
@@ -1,6 +1,6 @@
-import redis
+from redis import Redis
 from app.rediscli import get_redis
 from typing import Optional
 
-def get_redis_client(db: Optional[int] = None) -> redis.Redis:
+def get_redis_client(db: Optional[int] = None) -> Redis:
     return get_redis()
